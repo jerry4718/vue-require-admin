df5c1e5d675c506d20aa24aa73715d5a178d20c0
jerry <jerry4718@qq.com>
2020-04-02 21:55:56 +0800

    本地依赖换为bower管理，ajax移入util目录

--- src/config/config.js
+++ src/config/config.js
@@ -1,16 +1,16 @@
 define({
     paths: {
-        Vue: 'lib/vue-2.2.2',
-        VueRouter: 'lib/vue-router-3.1.6',
-        VueI18n: 'lib/vue-i18n-8.15.7',
-        MgrMain: 'components/main/main.component',
-        ajax: 'lib/ajax',
+        Vue: 'bower_components/vue/dist/vue.min',
+        VueRouter: 'bower_components/vue-router/dist/vue-router.min',
+        VueI18n: 'bower_components/vue-i18n/dist/vue-i18n.min',
         NProgress: 'bower_components/nprogress/nprogress',
+        ajax: 'common/util/ajax',
+        MgrMain: 'components/main/main.component',
     },
     map: {
         '*': {
-            'css': 'lib/requirejs-css-0.1.10',
-            'text': 'lib/requirejs-text-2.0.14',
+            'css': 'bower_components/require-css/css',
+            'text': 'bower_components/text/text',
         },
     },
     shim: {
